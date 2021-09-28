9b134367a9996d9823e08632037c06de5a232b93
Victoria Luquet Tewari <victorialuquet@MacBook-Pro-de-Devendra.local>
2021-09-28 13:53:05 -0300

    tried new approach

--- google.js
+++ google.js
@@ -1,65 +1,79 @@
-const puppeteer = require('puppeteer');
-const fs = require('fs');
-const dotenv = require('dotenv');
-const { } = require('./controllers');
+const puppeteer = require("puppeteer");
+const fs = require("fs");
+const dotenv = require("dotenv");
+const {} = require("./controllers");
 
 dotenv.config();
 const url = "https://www.google.com/";
 
 async function getTable(page) {
-    let tbl, error = false;
-    try {
-        tbl = await page.$eval('div[data-name="stores"] table', (table) => {
-            return table;
-        });
-    } catch (err) {
-        console.log(err)
-        error = true;
-    }
+  let tbl,
+    error = false;
+  try {
+    tbl = await page.$eval('div[data-name="stores"] table', (table) => {
+      return table;
+    });
+  } catch (err) {
+    console.log(err);
+    error = true;
+  }
 
-    return { tbl, error };
+  // return { tbl, error };
+  if (!error && tbl) {
+    console.log(tbl);
+  } else if (error) {
+    console.log("There was an error!");
+  } else if (!error && !tbl) {
+    console.log("There is no table!");
+  }
 }
 
 async function run() {
-    const browser = await puppeteer.launch({
-        args: ["--no-sandbox"],
-        headless: false,
-        ignoreHTTPSErrors: true
-    });
+  const browser = await puppeteer.launch({
+    args: ["--no-sandbox"],
+    headless: false,
+    ignoreHTTPSErrors: true,
+  });
 
-    const browserPage = await browser.newPage();  // opens a page
-    await browserPage.setViewport({
-        width: 1200,
-        height: 720,
-        deviceScaleFactor: 1,
-    })
-    await browserPage.goto(url, { waitUntil: 'networkidle2' });
+  const browserPage = await browser.newPage(); // opens a page
+  await browserPage.setViewport({
+    width: 1200,
+    height: 720,
+    deviceScaleFactor: 1,
+  });
+  await browserPage.goto(url, { waitUntil: "networkidle2" });
 
-    await browserPage.click('input[role="combobox"]');
-    await browserPage.$eval('input[role="combobox"]', (input) => {
-        input.value = "iphone 12";
-    });
+  await browserPage.click('input[role="combobox"]');
+  await browserPage.$eval('input[role="combobox"]', (input) => {
+    input.value = "iphone 12";
+  });
 
-    // await Promise.all([
-    //     browserPage.waitForNavigation({waitUntil: 'load'}),
-    //     browserPage.keyboard.press('Enter')
-    // ]);
+  // Alternative for lines 57 and 58
+  // await Promise.all([
+  //     browserPage.waitForNavigation({waitUntil: 'load'}),
+  //     browserPage.keyboard.press('Enter')
+  // ]);
 
-    await browserPage.keyboard.press('Enter');
-    await browserPage.waitForSelector('button[data-name="stores"]');
+  await browserPage.keyboard.press("Enter");
+  await browserPage.waitForSelector('button[data-name="stores"]');
 
-    await browserPage.click('button[data-name="stores"]');
-    let table;
+  await browserPage.click('button[data-name="stores"]');
 
-    // Add timeout inside while and leave while when timeout arives
-    try {
-        table = await getTable(browserPage);
-    } catch (err) {
-        console.log(err);
+  // Add timeout inside while and leave while when timeout arives
+  let gotToTimeout = false,
+    createdTimeout = false;
+
+  while (!gotToTimeout) {
+    if (!createdTimeout) {
+      setTimeout(async () => {
+        await getTable();
+        gotToTimeout = true;
+      }, 3000);
+      createdTimeout = true;
     }
+  }
 
-    console.log(table);
-    await browser.close();
+  await browser.close();
 }
 
 run();
